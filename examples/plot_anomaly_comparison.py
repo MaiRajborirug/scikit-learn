f0d7ee130016773bc25e57ea7ac831b81c8775fe
Mai <54995188+MaiRajborirug@users.noreply.github.com>
2020-03-02 07:05:31 -0500

    Add files via upload

--- examples/plot_anomaly_comparison.py
+++ examples/plot_anomaly_comparison.py
@@ -2,15 +2,18 @@
 ============================================================================
 Comparing anomaly detection algorithms for outlier detection on toy datasets
 ============================================================================
+
 This example shows characteristics of different anomaly detection algorithms
 on 2D datasets. Datasets contain one or two modes (regions of high density)
 to illustrate the ability of algorithms to cope with multimodal data.
+
 For each dataset, 15% of samples are generated as random uniform noise. This
 proportion is the value given to the nu parameter of the OneClassSVM and the
 contamination parameter of the other outlier detection algorithms.
 Decision boundaries between inliers and outliers are displayed in black
 except for Local Outlier Factor (LOF) as it has no predict method to be applied
 on new data when it is used for outlier detection.
+
 The :class:`sklearn.svm.OneClassSVM` is known to be sensitive to outliers and
 thus does not perform very well for outlier detection. This estimator is best
 suited for novelty detection when the training set is not contaminated by
@@ -18,9 +21,11 @@ outliers. That said, outlier detection in high-dimension, or without any
 assumptions on the distribution of the inlying data is very challenging, and a
 One-class SVM might give useful results in these situations depending on the
 value of its hyperparameters.
+
 :class:`sklearn.covariance.EllipticEnvelope` assumes the data is Gaussian and
 learns an ellipse. It thus degrades when the data is not unimodal. Notice
 however that this estimator is robust to outliers.
+
 :class:`sklearn.ensemble.IsolationForest` and
 :class:`sklearn.neighbors.LocalOutlierFactor` seem to perform reasonably well
 for multi-modal data sets. The advantage of
@@ -29,6 +34,7 @@ shown for the third data set, where the two modes have different densities.
 This advantage is explained by the local aspect of LOF, meaning that it only
 compares the score of abnormality of one sample with the scores of its
 neighbors.
+
 Finally, for the last data set, it is hard to say that one sample is more
 abnormal than another sample as they are uniformly distributed in a
 hypercube. Except for the :class:`sklearn.svm.OneClassSVM` which overfits a
@@ -36,8 +42,10 @@ little, all estimators present decent solutions for this situation. In such a
 case, it would be wise to look more closely at the scores of abnormality of
 the samples as a good estimator should assign similar scores to all the
 samples.
+
 While these examples give some intuition about the algorithms, this
 intuition might not apply to very high dimensional data.
+
 Finally, note that parameters of the models have been here handpicked but
 that in practice they need to be adjusted. In the absence of labelled data,
 the problem is completely unsupervised so model selection can be a challenge.
@@ -88,9 +96,9 @@ datasets = [
                **blobs_params)[0],
     make_blobs(centers=[[2, 2], [-2, -2]], cluster_std=[1.5, .3],
                **blobs_params)[0],
-    4. * (make_moons(n_samples=n_inliers, noise=.05, random_state=0)[0] -
+    4. * (make_moons(n_samples=n_samples, noise=.05, random_state=0)[0] -
           np.array([0.5, 0.25])),
-    14. * (np.random.RandomState(42).rand(n_inliers, 2) - 0.5)]
+    14. * (np.random.RandomState(42).rand(n_samples, 2) - 0.5)]
 
 # Compare given classifiers under given settings
 xx, yy = np.meshgrid(np.linspace(-7, 7, 150),
