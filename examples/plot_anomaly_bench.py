be5119b505167eae7d8c2893dc02b0d55fa682ce
Mai <54995188+MaiRajborirug@users.noreply.github.com>
2020-03-29 10:52:21 -0400

    render legend 2

--- examples/plot_anomaly_bench.py
+++ examples/plot_anomaly_bench.py
@@ -113,13 +113,13 @@ for dataset_idx, dataset_name in enumerate(datasets):
         fpr, tpr, thresholds = roc_curve(y, scoring)
         AUC = auc(fpr, tpr)
         label_ = str(model_name
-                     + ": ROC for %s (area = %0.3f, train-time: %0.2fs)"
+                     + ": ROC for %s (AUC = %0.3f, \u0394t: %0.2fs)"
                      % (dataset_name, AUC, fit_time))
         plt.plot(fpr, tpr, lw=1, label=label_)
 
     plt.xlim([-0.05, 1.05])
     plt.ylim([-0.05, 1.05])
-    plt.legend(loc="best")
+    plt.legend(loc="lower right")
     if dataset_idx == 0:
         plt.title("Receiver operating characteristic")
     if dataset_idx == len(datasets) - 1:
